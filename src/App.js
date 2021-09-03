e7db28777ab0fa8f802ea450ca2ef0fb4f78d327
Joao Pedro <jpresende502@gmail.com>
2021-09-03 18:53:06 -0300

    feat: creating metamask connection

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components"
+
+const Wrapper = styled.main`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+`
+const Button = styled.button`
+  background-color: ${ props => 
+    props.color
+  };
+  cursor: pointer;
+  border: 13px;
+  outline: none;
+  color: #fff;
+  padding: 16px;
+  font-size: 1.8rem;
+`
+const Text = styled.h1`
+  color: ${ props => props.color};
+  font-size: 1.6rem;
+  font-family: Ubuntu;
+`
+
 
 function App() {
+  const [useWallet, setWallet] = useState("")
+  const ethereum = window.ethereum
+  
+  async function connectWallet() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    setWallet(accounts[0])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      {ethereum ? 
+        <div>
+          {!useWallet && <Button onClick={connectWallet} color="#037DD6">Connect Ethereum Wallet!</Button>}
+          {useWallet && 
+            <div>
+              {/* <Button onClick={disconnectWallet} color="#BB2D3B">Disconnect Wallet</Button> */}
+              <Text color="#000">Your ethereum address: {useWallet}</Text>
+            </div>
+          }
+        </div>
+      :
+        <Text color="#ff000">Ops... You dont have a Ethereum Wallet in your browser :/ </Text>
+      }
+    </Wrapper>
   );
 }
 
